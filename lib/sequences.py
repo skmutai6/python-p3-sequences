712c1a3bc330d7c420b01715ff07926d7e62f4a1
Shadrack <skmutai6@gmail.com>
2024-05-24 22:23:35 +0300

    lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        return []
+    elif length == 1:
+        return [0]
+    
+    fib = [0, 1]
+    for i in range(2, length):
+        fib.append(fib[-1] + fib[-2])
+    return fib
+length = 1
+print(print_fibonacci(length))
+
+# index 0 + index 1 = index 3
+
+
